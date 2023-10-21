77dc3ffa80fead817bd721d219c69ec77a415f4d
jan99 <jan99@DESKTOP-6APHCUQ>
2023-10-20 23:40:45 -0500

    Agregar archivos de proyecto.

---
+++ loginnn/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace loginnn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btniniciar = new System.Windows.Forms.Button();
+            this.btnsalir = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtcontraseña = new System.Windows.Forms.TextBox();
+            this.txtusuario = new System.Windows.Forms.TextBox();
+            this.lblcontraseña = new System.Windows.Forms.Label();
+            this.lblusuario = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btniniciar
+            // 
+            this.btniniciar.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btniniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btniniciar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btniniciar.Location = new System.Drawing.Point(168, 125);
+            this.btniniciar.Margin = new System.Windows.Forms.Padding(2);
+            this.btniniciar.Name = "btniniciar";
+            this.btniniciar.Size = new System.Drawing.Size(105, 36);
+            this.btniniciar.TabIndex = 24;
+            this.btniniciar.Text = "Iniciar";
+            this.btniniciar.UseVisualStyleBackColor = false;
+            this.btniniciar.Click += new System.EventHandler(this.btniniciar_Click);
+            // 
+            // btnsalir
+            // 
+            this.btnsalir.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnsalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsalir.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnsalir.Location = new System.Drawing.Point(319, 125);
+            this.btnsalir.Margin = new System.Windows.Forms.Padding(2);
+            this.btnsalir.Name = "btnsalir";
+            this.btnsalir.Size = new System.Drawing.Size(105, 36);
+            this.btnsalir.TabIndex = 23;
+            this.btnsalir.Text = "Salir";
+            this.btnsalir.UseVisualStyleBackColor = false;
+            this.btnsalir.Click += new System.EventHandler(this.btnsalir_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(11, 21);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 120);
+            this.button1.TabIndex = 22;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // txtcontraseña
+            // 
+            this.txtcontraseña.Location = new System.Drawing.Point(268, 71);
+            this.txtcontraseña.Margin = new System.Windows.Forms.Padding(2);
+            this.txtcontraseña.MaxLength = 50;
+            this.txtcontraseña.Name = "txtcontraseña";
+            this.txtcontraseña.Size = new System.Drawing.Size(156, 20);
+            this.txtcontraseña.TabIndex = 20;
+            // 
+            // txtusuario
+            // 
+            this.txtusuario.Location = new System.Drawing.Point(268, 40);
+            this.txtusuario.Margin = new System.Windows.Forms.Padding(2);
+            this.txtusuario.MaxLength = 50;
+            this.txtusuario.Name = "txtusuario";
+            this.txtusuario.Size = new System.Drawing.Size(156, 20);
+            this.txtusuario.TabIndex = 18;
+            // 
+            // lblcontraseña
+            // 
+            this.lblcontraseña.AutoSize = true;
+            this.lblcontraseña.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.lblcontraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcontraseña.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblcontraseña.Location = new System.Drawing.Point(165, 71);
+            this.lblcontraseña.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblcontraseña.Name = "lblcontraseña";
+            this.lblcontraseña.Size = new System.Drawing.Size(95, 18);
+            this.lblcontraseña.TabIndex = 21;
+            this.lblcontraseña.Text = "Contraseña";
+            // 
+            // lblusuario
+            // 
+            this.lblusuario.AutoSize = true;
+            this.lblusuario.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.lblusuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblusuario.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblusuario.Location = new System.Drawing.Point(192, 39);
+            this.lblusuario.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblusuario.Name = "lblusuario";
+            this.lblusuario.Size = new System.Drawing.Size(67, 18);
+            this.lblusuario.TabIndex = 19;
+            this.lblusuario.Text = "Usuario";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Blue;
+            this.ClientSize = new System.Drawing.Size(440, 191);
+            this.Controls.Add(this.btniniciar);
+            this.Controls.Add(this.btnsalir);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtcontraseña);
+            this.Controls.Add(this.txtusuario);
+            this.Controls.Add(this.lblcontraseña);
+            this.Controls.Add(this.lblusuario);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btniniciar;
+        private System.Windows.Forms.Button btnsalir;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtcontraseña;
+        private System.Windows.Forms.TextBox txtusuario;
+        private System.Windows.Forms.Label lblcontraseña;
+        private System.Windows.Forms.Label lblusuario;
+    }
+}
+
---
+++ loginnn/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace loginnn
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conex = new SqlConnection("server=localhost\\SQLEXPRESS;database=db_usuario; integrated security=true");
+        int intentosFallidos = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btniniciar_Click(object sender, EventArgs e)
+        {
+            conex.Open();
+            string consulta = "select * from registro where usuario='" + txtusuario.Text + "' and contraseña='" + txtcontraseña.Text + "'";
+            SqlCommand comando = new SqlCommand(consulta, conex);
+            SqlDataReader lector;
+            lector = comando.ExecuteReader();
+
+            if (string.IsNullOrEmpty(txtusuario.Text) || string.IsNullOrEmpty(txtcontraseña.Text))
+            {
+                MessageBox.Show("Por favor ingrese nombre de usuario y contraseña", "Error");
+            }
+            else
+            {
+                if (lector.HasRows)
+                {
+                    MessageBox.Show("Bienvenido");
+                    Form2 f2 = new Form2();
+                    f2.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    intentosFallidos++;
+                    MessageBox.Show("Nombre de usuario o contraseña incorrecto. Intentos fallidos: " + intentosFallidos);
+
+                    if (intentosFallidos >= 3)
+                    {
+                        MessageBox.Show("Tres intentos fallidos. Cerrando la aplicación.");
+                        Application.Exit();
+                    }
+                }
+            }
+
+            conex.Close();
+
+        }
+
+        private void btnsalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
+//CREATE DATABASE db_usuario 
+//use db_usuario 
+
+//CREATE TABLE registro
+//(usuario NVARCHAR(50) NOT NULL,
+//contraseña NVARCHAR(50) NOT NULL)
+//insert into registro values( 'lana2816', '1234')
+//insert into registro values( 'jan18', '1234')
